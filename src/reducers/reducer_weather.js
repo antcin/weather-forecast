be336b7be24a6d2e46183ae09c72d933fa0b6365
antcin <ciniglioantonio@gmail.com>
2018-05-31 12:56:26 +0100

    map props to render helper and fix concat in weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat([state: action.payload.data]);
+      return state.concat([action.payload.data]);
       return [ action.payload.data, ...state ]; // [ city, city, city] NOT [city, [city, city]]
   }
   return state;
