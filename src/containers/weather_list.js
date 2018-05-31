f27e52102980f28e022b8946eb6cfb5075a7a822
antcin <ciniglioantonio@gmail.com>
2018-05-31 14:07:34 +0100

    start integrating google map

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,7 +6,7 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp;
+    const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
