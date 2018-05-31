4024bacc043b562e4f76135486d38a3df4530ebc
antcin <ciniglioantonio@gmail.com>
2018-05-31 10:57:32 +0100

    create ajax request to API

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this) //"this (instance of SearchBar) has a function called onInputChange, bind it to this (SearchBar) and then replace onInputChange with this new bound instance of this function"
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange(event) {
@@ -17,6 +21,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     //we need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -31,6 +37,13 @@ export default class SearchBar extends Component {
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
-    )
+    );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// we are passing null because whenever we pass a function that is supposed to map our dispatch to props of our containers, it always goes in as our second argument. "No state needed in here"
+export default connect(null, mapDispatchToProps)(SearchBar);
